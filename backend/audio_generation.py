150341643826b24858053a7f7213558d2d268851
RKsAuto <rishi125.works.here@gmail.com>
2026-02-18 02:32:25 +0530

    added gen_aud class and audio_generator function within it

--- backend/audio_generation.py
+++ backend/audio_generation.py
@@ -1,7 +1,11 @@
-import pydub
-import gtts
 
+from gtts import gtts
 class gen_aud():
-    def audio_generator(self, test:str):
+    def audio_generator(self, text:str):
+        aud = gtts(text)
         
         return aud
+
+if name == "_main_":
+    gen = gen_aud()
+    aud = gen_aud.audio_generator("I am Rish")
